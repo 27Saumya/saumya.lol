d57d9c8ef88a0a4f22947b5de781d2f6b0e6e055
Saumya <patelsaumya767@gmail.com>
2024-02-18 01:31:37 +0530

    Add Meta Tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,12 @@
   import Main from "../components/Hero.svelte";
 </script>
 
+<svelte:head>
+  <title>Saumya</title>
+  <meta property="og:title" content="Saumya" />
+  <meta property="og:description" content="Yet another boring developer!" />
+  <meta property="og:image" content="/images/saumya-card.png" />
+  <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
 <Main />
